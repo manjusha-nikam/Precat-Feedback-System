d1469e8d660adeedceb0419a4a305eb59c64db79
samruddhiphadnis <samruddhi.phadnis@sunbeaminfo.com>
2024-02-23 14:35:02 +0530

    student signin functionality file added

---
+++ Server/routes/feedback.js
@@ -0,0 +1,11 @@
+const express = require("express")
+const router = express.Router()
+const db = require("../db")
+const utils = require("../utils")
+
+
+
+
+
+
+  module.exports = router
---
+++ Server/routes/student.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const router = express.Router()
+const db = require('../db')
+const utils = require('../utils')
+
+
+
+
+router.post('/signin', (request, response) => {
+  const { email, password } = request.body
+console.log(email,password)
+  // encrypt the password
+  // const encryptedPassword = cryptoJs.MD5(password)
+
+  const query = `select id, firstName, lastName, email, mobile from student where email = '${email}' and password = '${password}'`
+
+  db.query(query, (error, users) => {
+    if (error) {
+      // when there is an error from MySQL side
+      response.send(utils.createError(error))
+    } else {
+      // user with correct email and password does not exist
+      if (users.length == 0) {
+        response.send(utils.createError('invalid user name or password'))
+      } else {
+        response.send(utils.createResult(error, users[0]))
+      }
+    }
+  })
+})
+
+
+
+
+
+
+
+
+module.exports = router
