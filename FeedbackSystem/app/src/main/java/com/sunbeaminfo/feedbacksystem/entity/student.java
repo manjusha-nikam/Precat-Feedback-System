bc8d793db4ce855c46bdbe83fd3e137d4835f6c3
sonal15495 <sonal.shinde@sunbeaminfo.com>
2024-02-23 16:58:47 +0530

    Login Screen Of Student Feedback_System

---
+++ FeedbackSystem/app/src/main/java/com/sunbeaminfo/feedbacksystem/entity/student.java
@@ -0,0 +1,75 @@
+package com.sunbeaminfo.feedbacksystem.entity;
+
+import java.io.Serializable;
+
+public class student implements Serializable {
+
+    private int id;
+    private String  firstName;
+    private String lastName;
+    private String email;
+    private String mobile;
+    private String password;
+
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "student{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", mobile='" + mobile + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
---
+++ FeedbackSystem/app/src/main/java/com/sunbeaminfo/feedbacksystem/utility/FeedBackSystemConstants.java
@@ -0,0 +1,8 @@
+package com.sunbeaminfo.feedbacksystem.utility;
+
+public class FeedBackSystemConstants {
+    public static final String SHARED_PREFERENCE_FILE_NAME = "feedback_sp";
+    public static final String LOGIN_STATUS = "login_status";
+    public static final String USER_ID = "id";
+
+}
