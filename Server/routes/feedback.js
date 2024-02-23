69f68547b7bd4e4f576c8ae4951e1e6e1e4db7da
samruddhiphadnis <samruddhi.phadnis@sunbeaminfo.com>
2024-02-23 14:37:01 +0530

    all schedule functionality added

--- Server/routes/feedback.js
+++ Server/routes/feedback.js
@@ -6,6 +6,12 @@ const utils = require("../utils")
 
 
 
-
+  
+router.get('/schedule', (request, response) => {
+    const query = `select * from schedule `
+    db.query(query, (error, result) => {
+      response.send(utils.createResult(error, result))
+    })
+  })
 
   module.exports = router
